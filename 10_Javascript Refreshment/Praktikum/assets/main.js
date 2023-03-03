ab874059f892b57b5ab0530b2abe8a8f0d369186
AlfinhiHD <alfinhihd@gmail.com>
2023-03-03 15:12:54 +0700

    added changes

--- 10_Javascript Refreshment/Praktikum/assets/main.js
+++ 10_Javascript Refreshment/Praktikum/assets/main.js
@@ -6,6 +6,9 @@ const radioButtons = document.getElementsByName('freshness');
 const descInput = document.querySelector('#productDesc');
 const priceInput = document.querySelector('#productPrice');
 
+//button class
+// const submitButton = document.getElementById('submitButton');
+
 // Errors id on tag p 
 const nameError = document.getElementById('nameError');
 const cathegoryError = document.getElementById('cathegoryError');
@@ -38,45 +41,73 @@ form.addEventListener('submit', function (event) {
 
     // Validasi input Cathegory
     if (cathegoryInput.value === '') {
-        cathegoryError.textContent = 'Kota tidak boleh kosong.';
+        cathegoryError.textContent = 'Kategori tidak boleh kosong.';
+        cathegoryInput.classList.remove("is-valid");
+        cathegoryInput.classList.add("is-invalid");
     } else {
         cathegoryError.textContent = '';
+        cathegoryInput.classList.remove("is-invalid");
+        cathegoryInput.classList.add("is-valid");
     }
 
     // Validasi input File
     if (imageInput.value === '') {
         imageError.textContent = 'File tidak boleh kosong.';
+        imageInput.classList.remove("is-valid");
+        imageInput.classList.add("is-invalid");
     } else {
         imageError.textContent = '';
+        imageInput.classList.remove("is-invalid");
+        imageInput.classList.add("is-valid");
     }
 
     // Validasi input Freshness
     for (let i = 0; i < radioButtons.length; i++) {
         if (radioButtons[i].checked) {
             freshnessError.textContent = '';
+            selectedValue = radioButtons[i].value;
           break;
         } else {
             freshnessError.textContent = 'Jenis freshness harus dipilih.';
         }
-      }
+    }
 
     // Validasi input Dekripsi
     if (descInput.value === '') {
         descError.textContent = 'Deskripsi product tidak boleh kosong.';
+        descInput.classList.remove("is-valid");
+        descInput.classList.add("is-invalid");
     } else {
         descError.textContent = '';
+        descInput.classList.remove("is-invalid");
+        descInput.classList.add("is-valid");
     }
 
     // Validasi input price
+
     if (priceInput.value === '') {
         priceError.textContent = 'Harga product tidak boleh kosong.';
+        priceInput.classList.remove("is-valid");
+        priceInput.classList.add("is-invalid");
     } else {
         priceError.textContent = '';
+        priceInput.classList.remove("is-invalid");
+        priceInput.classList.add("is-valid");
     }
 
     // Jika semua input valid, submit form
     if (nameError.textContent === '' && cathegoryError.textContent === '' && imageError.textContent === '' && freshnessError.textContent === '' && descError.textContent === '' && priceError.textContent === '') {
         form.submit();
+
+        alert("Data Produk\n \nNama : " + nameInput.value 
+        + "\nCathegory : " + cathegoryInput.value
+        + "\nImage : " + imageInput.value
+        + "\nFreshness : " + selectedValue
+        + "\nDecription : " + descInput.value
+        + "\nPrice : " + priceInput.value)
     }
-    
+
 });
+
+//Tambahan button disabled
+//Fungsi onkeyup
