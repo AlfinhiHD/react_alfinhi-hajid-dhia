fa351479df9a058f9d8bc1cb24a7bee04852853c
AlfinhiHD <alfinhihd@gmail.com>
2023-03-31 20:50:52 +0700

    add context initiation

--- 19_React Form/Praktikum/src/App.jsx
+++ 19_React Form/Praktikum/src/App.jsx
@@ -7,6 +7,7 @@ import CreateProduct from './pages/CreateProduct/CreateProduct';
 import LandingPage from './pages/LandingPage/LandingPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import DetailProduct from './pages/DetailProduct/DetailProduct';
+import ProductsContext from './context/ProductsContext';
 
 
 
@@ -17,19 +18,20 @@ function App() {
   // }, [])
 
   const [datas, setDatas] = useState([])
-  // console.log(datas)
-
+  const value = {datas, setDatas}
 
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/getstarted" element={<CreateProduct setData={setDatas}  />} />
-        <Route path="/detailproduct/:productId" element={<DetailProduct product={datas} />} />
-        {/* render={(props) => <DetailProduct productId={props.match.params.productId} */}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <ProductsContext.Provider value = {value}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/getstarted" element={<CreateProduct setData={setDatas}  />} />
+          <Route path="/detailproduct/:productId" element={<DetailProduct product={datas} />} />
+          {/* render={(props) => <DetailProduct productId={props.match.params.productId} */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </ProductsContext.Provider>
   )
 
 }
