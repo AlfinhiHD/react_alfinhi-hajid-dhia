41d2aea30adfaaa4c8333d7025a03c0a21307257
AlfinhiHD <alfinhihd@gmail.com>
2023-04-14 02:40:24 +0700

    add changes

--- 27_GraphQL Query and Mutation/Praktikum/src/pages/DetailProduct/DetailProduct.jsx
+++ 27_GraphQL Query and Mutation/Praktikum/src/pages/DetailProduct/DetailProduct.jsx
@@ -1,12 +1,36 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { gql, useMutation, useQuery } from "@apollo/client"
 import { useEffect } from "react";
 import Input from "../../elements/Input/Input";
 import Button from "../../elements/Button/Button";
 import { useState } from "react";
 
+const UpdateProduct = gql`
+    mutation MyMutation($id: String!, $object: Product_set_input!) {
+        update_Product_by_pk(pk_columns: {id: $id}, 
+        _set: $object) {
+          id
+          name
+          price
+        }
+      }
+    `;
+
+const HapusProduct = gql`
+        mutation MyQuery($id: String!) {
+        delete_Product_by_pk(id: $id) {
+            id
+        }
+        }
+    `;
+
 
 const DetailProduct = () => {
+
+    const [hapusProduct] = useMutation(HapusProduct);
+    const [updateProduct] = useMutation(UpdateProduct);
+
+    const navigate = useNavigate();
 
     const formData = {
         productId: "",
@@ -46,35 +70,32 @@ const DetailProduct = () => {
           freshness
         }
     }`;
-    
+
     const { data, loading, error } = useQuery(GetProductList, {
+        onCompleted: ({ Product: data }) => setDataEdit({
+            productId: data[0].id,
+            productName: data[0].name,
+            productCathegory: data[0].category,
+            productImage: data[0].image,
+            productFreshness: data[0].freshness,
+            productDesc: data[0].description,
+            productPrice: data[0].price
+        })
     })
 
     if (loading) {
         return <div>Loading...</div>;
-    } if (error) {
+    }
+
+    if (error) {
         return <div>Error: {error.message}</div>;
-    } if (!data?.Product?.length) {
+    }
+
+    if (!data?.Product?.length) {
         return <div>Product not found</div>;
     }
 
-    console.log(data)
-
-    const product = data.Product[0];
-
-
-    // const item = data.Product.find((item) => item.id === productId)
-
-    setDataEdit({
-        productId: product.id,
-        productName: product.name,
-        productCathegory: product.category,
-        productImage: product.image,
-        productFreshness: product.freshness,
-        productDesc: product.description,
-        productPrice: product.price
-    })
-
+    console.log(dataEdit)
 
     const handleInput = e => {
         const name = e.target.name;
@@ -86,20 +107,48 @@ const DetailProduct = () => {
         }))
     }
 
-     const handleSubmit = e => {
+    const handleDelete = (id) => {
+        console.log(id)
+        if (window.confirm('Are you sure you want to delete this item?')) {
+            hapusProduct({
+                variables: {
+                    id: id
+                }
+            })
+            navigate('/')
+        }
+    }
+
+    const handleEdit = () => {
+        console.log(dataEdit)
+        updateProduct({
+            variables: {
+                id: dataEdit.productId,
+                object: {
+                    name: dataEdit.productName,
+                    price: dataEdit.productPrice,
+                    freshness: dataEdit.productFreshness,
+                    description: dataEdit.productDesc,
+                    category: dataEdit.productCathegory
+                }
+            },
+        });
+    }
+
+    const handleSubmit = e => {
         e.preventDefault();
 
         if (!data.productName) {
             errorEdit.productName = "Name is required";
         }
         if (!data.productCathegory) {
-            errorEdit.productCathegory= "Category is required";
+            errorEdit.productCathegory = "Category is required";
         }
-        if (!data.productImage) {
-            errorEdit.productImage= "Image is required";
-        }
+        // if (!data.productImage) {
+        //     errorEdit.productImage = "Image is required";
+        // }
         if (!data.productDesc) {
-            errorEdit.productDesc= "Desc is required";
+            errorEdit.productDesc = "Desc is required";
         }
         if (!data.productPrice) {
             errorEdit.productPrice = "Price is required";
@@ -112,19 +161,17 @@ const DetailProduct = () => {
         }
     }
 
-
-
     return (
         <div className="container">
             {/* <div className="container mt-5">
                 <h2>Detail Product</h2>
-                <h3>{product.id}</h3>
-                <h5> Product Name : {product.name}</h5>
-                <h5> Product Cathegory : {product.category}</h5>
-                <h5> Product Image : {product.image}</h5>
-                <h5> Product Freshness : {product.freshness}</h5>
-                <h5> Product Description : {product.description}</h5>
-                <h5> Product Price : {product.price}</h5>
+                <h3>{data.id}</h3>
+                <h5> Product Name : {data.name}</h5>
+                <h5> Product Cathegory : {data.category}</h5>
+                <h5> Product Image : {data.image}</h5>
+                <h5> Product Freshness : {data.freshness}</h5>
+                <h5> Product Description : {data.description}</h5>
+                <h5> Product Price : {data.price}</h5>
             </div> */}
             <form onSubmit={handleSubmit} className="container mt-5 w-50" id="productForm">
                 <h2>Detail Product</h2>
@@ -136,7 +183,7 @@ const DetailProduct = () => {
                         type="text"
                         name="productName"
                         className="form-control"
-                        value={dataEdit.productName}
+                        value={dataEdit?.productName}
                         onChange={handleInput}
                     />
                     <small id="nameError" className="text-danger">
@@ -149,7 +196,7 @@ const DetailProduct = () => {
                     </label>
                     <select
                         name="productCathegory"
-                        value={dataEdit.productCathegory}
+                        value={dataEdit?.productCathegory}
                         className="form-select"
                         aria-label="Default select example"
                         onChange={handleInput}
@@ -172,10 +219,10 @@ const DetailProduct = () => {
                     <label className="form-label" htmlFor="productImage">
                         Image of Product
                     </label>
+                    <img src={dataEdit.productImage}></img>
                     <input
                         className="form-control"
                         name="productImage"
-                        // value={product.image}
                         type="file"
                         onChange={handleInput}
                         accept="image/*"
@@ -187,6 +234,7 @@ const DetailProduct = () => {
                     </label>
                     <div className="form-check">
                         <input
+                            checked={dataEdit.productFreshness == "Brand New"} 
                             id="new"
                             name="productFreshness"
                             className="form-check-input"
@@ -199,6 +247,7 @@ const DetailProduct = () => {
                     </div>
                     <div className="form-check">
                         <input
+                            checked={dataEdit.productFreshness == "Second Hand"} 
                             id="second"
                             name="productFreshness"
                             className="form-check-input"
@@ -211,6 +260,7 @@ const DetailProduct = () => {
                     </div>
                     <div className="form-check">
                         <input
+                            checked={dataEdit.productFreshness == "Refurbished"} 
                             id="refurbished"
                             name="productFreshness"
                             className="form-check-input"
@@ -230,7 +280,7 @@ const DetailProduct = () => {
                         rows={5}
                         className="form-control"
                         name="productDesc"
-                        value={dataEdit.productDesc}
+                        value={dataEdit?.productDesc}
                         onChange={handleInput}
                     />
                 </div>
@@ -242,14 +292,19 @@ const DetailProduct = () => {
                         type="number"
                         className="form-control"
                         name="productPrice"
-                        value={dataEdit.productPrice}
+                        value={dataEdit?.productPrice}
                         onChange={handleInput}
                     />
                 </div>
                 <Button
-                    type="submit"
-                    className="btn btn-primary w-100 mt-5"
-                    label="Submit"
+                    onClick={handleEdit}
+                    className="btn btn-warning mt-5"
+                    label="Edit"
+                />
+                <Button
+                    onClick={() => handleDelete(dataEdit.productId)}
+                    className="btn btn-danger mt-5"
+                    label="Delete"
                 />
             </form>
         </div>
